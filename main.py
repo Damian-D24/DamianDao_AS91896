468fa020049e0f1cded639ccb204c9cd99558928
Damian-D24 <23154@students.mrgs.school.nz>
2026-04-28 09:54:56 +1200

    Test

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+from tkinter import *
+from tkinter import messagebox
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+root = Tk()
 
+# Adjust size of windows
+root.geometry("1280x720")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Adding an image file
+bg = PhotoImage(file="images/homepage.png")
 
+# Create canvas
+canvas1=Canvas(root, width=1280, height=720)
+canvas1.pack(fill="both", expand=True)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Display the background
+canvas1.create_image(0, 0, image=bg, anchor="nw")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Adding buttons
+button1=Button(root, text="Let's Start")
+
+# Display Buttons
+button1_canvas=canvas1.create_window(
+    100,
+    10,
+    anchor="nw",
+    window=button1
+)
+
+button1.place(x=464.3, y=57)
+
+button1.config(width=16, height=2)
+
+root.mainloop()
