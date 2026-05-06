3b6f27f3b6e3dd1c6ee9f143bb8a827c98457092
Damian-D24 <23154@students.mrgs.school.nz>
2026-05-06 14:59:30 +1200

    6/05/2026

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import PIL
 import customtkinter as ctk
 import pyglet
 from pathlib import Path
 from tkinter import *
+from PIL import Image
 from tkinter import messagebox
 import random
 
-app = ctk.CTk()
+root = ctk.CTk()
 
 # Global Variables
 names = []
@@ -57,7 +59,7 @@ main_font=ctk.CTkFont(family=balsamiqsans, size=50, weight="bold")
 exit_font=ctk.CTkFont(family=balsamiqsans, size=25)
 
 # Adjust size of windows
-app.geometry("1280x720")
+root.geometry("1280x720")
 
 #
 # Defined Functions
@@ -65,7 +67,7 @@ app.geometry("1280x720")
 
 # Close Window
 def close_window():
-    app.destroy()
+    root.destroy()
 
 # Start
 
@@ -73,33 +75,24 @@ def close_window():
 # SCREEN 1
 #
 class QuizStart:
-    # Adding an image file for the background
-    bg = PhotoImage(file="images/homepage.png")
-    # Create canvas
-    canvas1=Canvas(app, width=1280, height=720)
-    canvas1.pack(fill="both", expand=True)
-    # Display the background
-    canvas1.create_image(0, 0, image=bg, anchor="nw")
-    # Adding buttons
-    button1=ctk.CTkButton(app, text="Let's Start", bg_color="white", fg_color="#62c370", font=main_font, text_color="white", width=322, height=100, corner_radius=50)
-    button2=ctk.CTkButton(app, text="Exit", bg_color="#e46a4a", fg_color="white", font=exit_font, text_color="black", width=100, height=18, corner_radius=8, command=close_window)
+    def __init__(self, parent):
+        # Adding an image file for the background
+        bg = PIL.Image.open("images/homepage.png")
+        # Create canvas
+        frame1=Frame(root, width=1280, height=720)
+        frame1.pack(fill="both", expand=True)
+        # Display the background
+        bg_image=ctk.CTkImage(light_image=bg,dark_image=bg, size=(1280, 720))
+        bg_label = ctk.CTkLabel(root, image=bg_image, text="")
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        # Adding buttons
+        button1=ctk.CTkButton(root, text="Let's Start", bg_color="white", fg_color="#62c370", font=main_font, text_color="white", width=322, height=100, corner_radius=50)
+        button2=ctk.CTkButton(root, text="Exit", bg_color="#e46a4a", fg_color="white", font=exit_font, text_color="black", width=100, height=18, corner_radius=8, command=close_window)
 
-    # Display "Let's Start" Button
-    button1_canvas=canvas1.create_window(
-        100,
-        10,
-        anchor="nw",
-        window=button1
-    )
-    button1.place(x=479, y=582, anchor="sw")
+        # Display "Let's Start" Button
+        button1.place(x=479, y=582, anchor="sw")
 
-    # Display "Exit" Button
-    button2_canvas=canvas1.create_window(
-        100,
-        10,
-        anchor="nw",
-        window=button2
-    )
-    button2.place(x=1152, y=72, anchor="sw")
+        # Display "Exit" Button
+        button2.place(x=1152, y=72, anchor="sw")
 
-app.mainloop()
+root.mainloop()
