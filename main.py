bb7890785c3db864495f535e942a83355291b8ca
Damian-D24 <23154@students.mrgs.school.nz>
2026-05-07 13:29:27 +1200

    7/05/2026

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ pyglet.font.add_file(str(font_path))
 balsamiqsans = "Balsamiq Sans"
 main_font=ctk.CTkFont(family=balsamiqsans, size=50, weight="bold")
 exit_font=ctk.CTkFont(family=balsamiqsans, size=25)
+main_exit_font=ctk.CTkFont(family=balsamiqsans, size=28)
 
 # Adjust size of windows
 root.geometry("1280x720")
@@ -76,23 +77,57 @@ def close_window():
 #
 class QuizStart:
     def __init__(self, parent):
-        # Adding an image file for the background
-        bg = PIL.Image.open("images/homepage.png")
+        self.parent = parent
         # Create canvas
-        frame1=Frame(root, width=1280, height=720)
-        frame1.pack(fill="both", expand=True)
+        self.frame=Frame(parent, width=1280, height=720)
+        self.frame.pack(fill="both", expand=True)
         # Display the background
+        # Adding background images
+        bg = PIL.Image.open("images/homepage.png")
         bg_image=ctk.CTkImage(light_image=bg,dark_image=bg, size=(1280, 720))
-        bg_label = ctk.CTkLabel(root, image=bg_image, text="")
-        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        self.bg_label = ctk.CTkLabel(self.frame, image=bg_image, text="")
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
         # Adding buttons
-        button1=ctk.CTkButton(root, text="Let's Start", bg_color="white", fg_color="#62c370", font=main_font, text_color="white", width=322, height=100, corner_radius=50)
-        button2=ctk.CTkButton(root, text="Exit", bg_color="#e46a4a", fg_color="white", font=exit_font, text_color="black", width=100, height=18, corner_radius=8, command=close_window)
-
+        self.button1=ctk.CTkButton(self.frame, text="Let's Start",
+                                   bg_color="white", fg_color="#62c370", font=main_font, text_color="white",
+                                   width=322, height=100, corner_radius=50, command=self.start_quiz)
+        self.button2=ctk.CTkButton(self.frame, text="Exit",
+                                   bg_color="#e46a4a", fg_color="white", font=exit_font, text_color="black",
+                                   width=100, height=18, corner_radius=8, command=close_window)
         # Display "Let's Start" Button
-        button1.place(x=479, y=582, anchor="sw")
+        self.button1.place(x=479, y=582, anchor="sw")
 
         # Display "Exit" Button
-        button2.place(x=1152, y=72, anchor="sw")
+        self.button2.place(x=1152, y=72, anchor="sw")
 
+    def start_quiz(self):
+        self.frame.destroy()
+        Quiz(self.parent)
+
+class Quiz:
+    def __init__(self, parent):
+        self.parent = parent
+        # Create canvas
+        self.frame=Frame(parent, width=1280, height=720)
+        self.frame.pack(fill="both", expand=True)
+        # Display the background
+        # Adding background images
+        bg = PIL.Image.open("images/question.png")
+        bg_image=ctk.CTkImage(light_image=bg,dark_image=bg, size=(1280, 720))
+        self.bg_label = ctk.CTkLabel(self.frame, image=bg_image, text="")
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        # Adding buttons
+        self.button1=ctk.CTkButton(self.frame, text="Let's Start",
+                                   bg_color="white", fg_color="#62c370", font=main_font, text_color="white",
+                                   width=322, height=100, corner_radius=50)
+        self.button2=ctk.CTkButton(self.frame, text="Exit",
+                                   bg_color="#e46a4a", fg_color="white", font=main_exit_font, text_color="black",
+                                   width=94, height=14, corner_radius=10, command=close_window)
+        # Display "Let's Start" Button
+        self.button1.place(x=479, y=582, anchor="sw")
+
+        # Display "Exit" Button
+        self.button2.place(x=1080, y=154, anchor="sw")
+
+quiz_instance = QuizStart(root)
 root.mainloop()
