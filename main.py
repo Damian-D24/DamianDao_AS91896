542f153328aa7b4192166b69f2eb0ed536d8154d
Damian-D24 <23154@students.mrgs.school.nz>
2026-05-19 09:28:05 +1200

    Uploaded new background images for quiz, added highlight colour for selected answer options, and fixed quiz randomisation

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ qnum = 0
 questions_answers = {
 1: ["What kind of bird is this?",
     "Sparrow", "Pīwakawaka", "Miromiro", "Kea",
-    "Pīwakawaka", 2, "images/birds/piwakawaka.jpg"],
+    "Pīwakawaka", 2, "images/birds/piwakawaka.png"],
 2: ["What kind of bird is this?",
     "Kea", "Kākāpō", "Tūī", "Kererū",
     "Kererū", 4, "images/birds/kereru.png"],
 3: ["What kind of bird is this?",
     "Pukeko", "Miromiro", "Takahē", "Kea",
-    "Pukeko", 1, "images/birds/pukeko.jpg"],
+    "Pukeko", 1, "images/birds/pukeko.png"],
 4: ["What kind of bird is this?",
     "Kererū", "Kea", "Kākāpō", "Kiwi",
     "Kea", 2, "images/birds/kea.png"],
@@ -35,19 +35,19 @@ questions_answers = {
     "Tūī", 3, "images/birds/tui.png"],
 6: ["What kind of bird is this?",
     "Mohua", "Sparrow", "Tūī", "Miromiro",
-    "Mohua", 1, "images/birds/mohua.jpg"],
+    "Mohua", 1, "images/birds/mohua.png"],
 7: ["What kind of bird is this?",
     "Black Robin", "Miromiro", "Riroriro", "Tuke",
-    "Riroriro", 3, "images/birds/riroriro.jpg"],
+    "Riroriro", 3, "images/birds/riroriro.png"],
 8: ["What kind of bird is this?",
     "Pukeko", "Takahē", "Kākā", "Weka",
-    "Takahē", 2, "images/birds/takahe.jpg"],
+    "Takahē", 2, "images/birds/takahe.png"],
 9: ["What kind of bird is this?",
     "Ruru", "Kōkako", "Penguin", "Kākāpō",
     "Kākāpō", 4, "images/birds/kakapo.png"],
 10: ["What kind of bird is this?",
     "Kiwi", "Albatross", "Kakī", "Miromiro",
-    "Kiwi", 1, "images/birds/kiwi.jpg"],
+    "Kiwi", 1, "images/birds/kiwi.png"],
 }
 
 # Loading Fonts
@@ -132,6 +132,8 @@ class QuizStart:
 class Quiz:
     def __init__(self, parent):
         self.parent = parent
+        # Pick a question before using qnum
+        randomiser()
         # Create Frame
         self.frame=Frame(parent, width=1280, height=720)
         self.frame.pack(fill="both", expand=True)
@@ -142,7 +144,7 @@ class Quiz:
         self.bg_label = ctk.CTkLabel(self.frame, image=bg_image, text="")
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
         #Adding Question Count
-        self.questioncounter = ctk.CTkLabel(self.frame, text=f"{len(asked) + 1}/10", font=questionnumber_font, fg_color="#62c370",
+        self.questioncounter = ctk.CTkLabel(self.frame, text=f"{len(asked)}/10", font=questionnumber_font, fg_color="#62c370",
                                             text_color="white")
         self.questioncounter.place(x=119, y=110)
         # Adding Exit Button
@@ -152,38 +154,72 @@ class Quiz:
         # Display "Exit" Button
         self.button2.place(x=1080, y=153, anchor="sw")
 
-        #Setting up Randomiser to Randomise Questions
-        randomiser()
         self.var = IntVar(value=0)
 
         # Adding Answer Option Buttons
-        self.option1=ctk.CTkRadioButton(self.frame,
+        self.option1 = ctk.CTkRadioButton(self.frame,
                     text=questions_answers[qnum][1],
                     variable=self.var, value=1, width=11, height=1,
                     font=answer_font,
-                    fg_color="#62c370", text_color="white", bg_color="#62c370", #command = lambda: selected_answer()
-                    ).place(x=109, y=257)
+                    fg_color="#62c370", text_color="white", bg_color="#62c370",
+                    command=self.highlight_selected)
+        self.option1.place(x=109, y=257)
 
-        self.option2=ctk.CTkRadioButton(self.frame,
+        self.option2 = ctk.CTkRadioButton(self.frame,
                     text=questions_answers[qnum][2],
                     variable=self.var, value=2, width=11, height=1,
                     font=answer_font,
-                    fg_color="#62c370", text_color="white"
-                    ).place(x=956, y=257)
+                    fg_color="#62c370", text_color="white", bg_color="#62c370",
+                    command=self.highlight_selected)
+        self.option2.place(x=956, y=257)
 
-        self.option3=ctk.CTkRadioButton(self.frame,
+        self.option3 = ctk.CTkRadioButton(self.frame,
                     text=questions_answers[qnum][3],
                     variable=self.var, value=3, width=11, height=1,
                     font=answer_font,
-                    fg_color="#62c370", text_color="white"
-                    ).place(x=109, y=470)
+                    fg_color="#62c370", text_color="white", bg_color="#62c370",
+                    command=self.highlight_selected)
+        self.option3.place(x=109, y=470)
 
-        self.option4=ctk.CTkRadioButton(self.frame,
+        self.option4 = ctk.CTkRadioButton(self.frame,
                     text=questions_answers[qnum][4],
                     variable=self.var, value=4, width=11, height=1,
                     font=answer_font,
-                    fg_color="#62c370", text_color="white"
-                    ).place(x=956, y=470)
+                    fg_color="#62c370", text_color="white", bg_color="#62c370",
+                    command=self.highlight_selected)
+        self.option4.place(x=956, y=470)
+
+        # Submit Button
+        self.submit_button = ctk.CTkButton(self.frame, text="Submit",
+                                           bg_color="white", fg_color="#62c370", font=main_font, text_color="white",
+                                           width=250, height=80, corner_radius=40,
+                                           command=self.submit_answer)
+        self.submit_button.place(x=640, y=640, anchor="center")
+
+
+    def submit_answer(self):
+        selected = self.var.get()
+        if selected == 0:
+            messagebox.showerror("Error", "Please select an answer.")
+            return
+        global score
+        if selected == questions_answers[qnum][6]:
+            score += 1
+        if len(asked) >= 10:
+            messagebox.showinfo("Quiz Complete", f"{names[-1]}, you scored {score}/10!")
+            close_window()
+        else:
+            self.frame.destroy()
+            Quiz(self.parent)
+
+    def highlight_selected(self):
+        options = [self.option1, self.option2, self.option3, self.option4]
+        selected = self.var.get()
+        for i, opt in enumerate(options, start=1):
+            if i == selected:
+                opt.configure(fg_color="#E2E037", bg_color="#E2E037", text_color="black")
+            else:
+                opt.configure(fg_color="#62c370", bg_color="#62c370", text_color="white")
 
 quiz_instance = QuizStart(root)
 root.mainloop()
